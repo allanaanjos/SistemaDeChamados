9c223edfee4bb56387d3d4eaf4f6b1fc5d14a85c
allan da silva <allananjosdasilva@gmail.com>
2025-11-28 14:50:18 -0300

    commit inicial

--- SistemaDeChamados.Application/obj/Debug/net8.0/SistemaDeChamados.Application.AssemblyInfo.cs
+++ SistemaDeChamados.Application/obj/Debug/net8.0/SistemaDeChamados.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaDeChamados.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3d85cf30ddf6e7a419887af63425ecfe65286d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaDeChamados.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaDeChamados.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- SistemaDeChamados.Infra/obj/Debug/net8.0/SistemaDeChamados.Infra.AssemblyInfo.cs
+++ SistemaDeChamados.Infra/obj/Debug/net8.0/SistemaDeChamados.Infra.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaDeChamados.Infra")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3d85cf30ddf6e7a419887af63425ecfe65286d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaDeChamados.Infra")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaDeChamados.Infra")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
